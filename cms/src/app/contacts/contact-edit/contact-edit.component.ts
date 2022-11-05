a6d9177fab7d808c74351a153f835355eebf1668
Savannah Dautel <sadautel@gmail.com>
2022-11-05 10:31:44 -0600

    Week08

--- cms/src/app/contacts/contact-edit/contact-edit.component.ts
+++ cms/src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,15 +1,110 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
 @Component({
-  selector: 'app-contact-edit',
+  selector: 'cms-contact-edit',
   templateUrl: './contact-edit.component.html',
-  styleUrls: ['./contact-edit.component.css']
+  styleUrls: ['./contact-edit.component.css'],
 })
 export class ContactEditComponent implements OnInit {
+  @ViewChild('f') contactForm!: NgForm;
+  subscription: Subscription;
+  originalContact: Contact;
+  groupContacts: Contact[] = [];
+  contact: Contact;
+  editMode: boolean = false;
+  id: string;
 
-  constructor() { }
+  constructor(
+    private contactService: ContactService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      let id = params.id;
+      if (!id || id == null) {
+        this.editMode = false;
+        return;
+      }
+
+      this.originalContact = this.contactService.getContact(id);
+
+      if (this.originalContact == null || !this.originalContact) {
+        return;
+      }
+      this.editMode = true;
+      this.contact = JSON.parse(JSON.stringify(this.originalContact));
+
+      if (this.contact.group) {
+        this.groupContacts = JSON.parse(JSON.stringify(this.contact.group));
+      }
+    });
   }
 
+  onSubmit(form: NgForm) {
+    const value = form.value;
+    const newContact = new Contact(
+      value.id,
+      value.name,
+      value.email,
+      value.phone,
+      value.imageUrl,
+      this.groupContacts
+    );
+    if (this.editMode && this.originalContact) {
+      this.contactService.updateContact(this.originalContact, newContact);
+    } else {
+      this.contactService.addContact(newContact);
+    }
+ 
+    this.router.navigate(['/contacts']);
+  }
+
+
+
+  onCancel() {
+    this.router.navigate(['/contacts']);
+  }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
+  }
+
+  isInvalidContact(newContact: Contact) {
+    if (!newContact) {
+      return true;
+    }
+    if (this.contact && newContact.id === this.contact.id) {
+      return true;
+    }
+
+    for (let i = 0; i < this.groupContacts.length; i++) {
+      if (newContact.id === this.groupContacts[i].id) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  addToGroup($event: any) {
+    const selectedContact: Contact = $event.dragData;
+    const invalidGroupContact = this.isInvalidContact(selectedContact);
+    if (invalidGroupContact) {
+      return;
+    }
+    this.groupContacts.push(selectedContact);
+  }
+
+  onRemoveItem(index: number) {
+    if (index < 0 || index >= this.groupContacts.length) {
+      return;
+    }
+    this.groupContacts.splice(index, 1);
+  }
 }
--- cms/src/app/documents/document-edit/document-edit.component.ts
+++ cms/src/app/documents/document-edit/document-edit.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-document-edit',
@@ -7,9 +12,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentEditComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('f') documentForm!: NgForm;
+  subscription: Subscription;
+  originalDocument: Document;
+  document: Document;
+  editMode: boolean = false;
 
-  ngOnInit(): void {
+  constructor(
+    private documentService: DocumentService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      let id = params.id;
+      if (!id || id == null) {
+        this.editMode = false;
+        return;
+      }
+
+      this.originalDocument = this.documentService.getDocument(id);
+
+      if (this.originalDocument == null || !this.originalDocument) {
+        return;
+      }
+      this.editMode = true;
+      this.document = JSON.parse(JSON.stringify(this.originalDocument));
+      console.log(this.document);
+
+    });
   }
 
+
+  onSubmit(form:NgForm){
+    const value = form.value;
+    const newDocument = new Document(
+      value.id,
+      value.name,
+      value.description,
+      value.url,
+      null
+    );
+    if (this.editMode && this.originalDocument) {
+      this.documentService.updateDocument(this.originalDocument, newDocument);
+    } else {
+      this.documentService.addDocument(newDocument);
+    }
+  
+    this.router.navigate(['/documents']);
+  }
+ 
+  onCancel(){
+    this.router.navigate(['/documents']);
+  }
 }
+
+
+
